3907601a13159e74069a261e32b3c04e122b4898
root <root@root.com>
2019-02-17 08:17:12 +0000

    add media files

--- OpenMap/OpenMap/settings.py
+++ OpenMap/OpenMap/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FILE_UPLOAD_PERMISSIONS = 0o744
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
